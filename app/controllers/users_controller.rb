6f5f4e968337e35d603f2e69523dd2b27fecf512
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2021-01-07 19:32:14 +0100

    user controller done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def update
+    @user = current_user
+    @user.update!(user_params)
+    create_request
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:phone_number, :biography)
+  end
+
+  def create_request
+    if @user.request == nil
+      Request.new()
+  end
+end
