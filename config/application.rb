a44812ca515d66f2d6eae81ee7481e9b072f9a92
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2021-01-13 22:21:30 +0100

    new local :fr, translations done

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module MCoworking
 
     config.active_job.queue_adapter = :sidekiq
 
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :fr
+    I18n.locale = :fr
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
